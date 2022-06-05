b34df765ee7ae5f50a34fdaacbc153f4f836ac7a
Hoang Nghia Viet <viethoangnghia@gmail.com>
2022-06-05 09:37:03 +0700

    add service in home page

--- src/pages/Home/HomeService/index.js
+++ src/pages/Home/HomeService/index.js
@@ -11,112 +11,112 @@ const HomeService = () => {
                 <h3 className={cx('tit-pub', 'center')}>Dịch vụ</h3>
                 <div className={cx('clear40')}></div>
                 <div className={cx('row', 'flex-wrap')}>
-                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')}>
-                        <div className={cx('list-gt', 'aos-init', 'aos-animate')} data-aos="flip-left">
+                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')} data-aos="flip-left">
+                        <div className={cx('list-gt', 'aos-init', 'aos-animate')}>
                             <div className={cx('eff-scale')}>
                                 <a href="#">
-                                    <img></img>
+                                    <img src="https://www.sbb.ch/content/dam/internet/sharedimages/personen/GA-Hund.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg"></img>
                                 </a>
                             </div>
                             <div className={cx('info-gt')}>
-                                <h4 className={cx('name-gt', 'text-uppercase')}>
-                                    <a href="#" className={cx('clblack')}>
+                                <h4 className={cx('name-gt')}>
+                                    <a href="#" className={cx('clblack', 'text-uppercase')}>
                                         THẨM ĐỊNH GIÁ BẤT ĐỘNG SẢN
                                     </a>
                                 </h4>
                             </div>
                         </div>
                     </div>
-                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')}>
-                        <div className={cx('list-gt', 'aos-init', 'aos-animate')} data-aos="flip-left">
+                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')} data-aos="flip-left">
+                        <div className={cx('list-gt', 'aos-init', 'aos-animate')}>
                             <div className={cx('eff-scale')}>
                                 <a href="#">
-                                    <img></img>
+                                    <img src="https://www.sbb.ch/content/dam/internet/sharedimages/personen/GA-Hund.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg"></img>
                                 </a>
                             </div>
                             <div className={cx('info-gt')}>
-                                <h4 className={cx('name-gt', 'text-uppercase')}>
-                                    <a href="#" className={cx('clblack')}>
+                                <h4 className={cx('name-gt')}>
+                                    <a href="#" className={cx('clblack', 'text-uppercase')}>
                                         THẨM ĐỊNH GIÁ ĐỘNG SẢN
                                     </a>
                                 </h4>
                             </div>
                         </div>
                     </div>
-                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')}>
-                        <div className={cx('list-gt', 'aos-init', 'aos-animate')} data-aos="flip-left">
+                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')} data-aos="flip-left">
+                        <div className={cx('list-gt', 'aos-init', 'aos-animate')}>
                             <div className={cx('eff-scale')}>
                                 <a href="#">
-                                    <img></img>
+                                    <img src="https://www.sbb.ch/content/dam/internet/sharedimages/personen/GA-Hund.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg"></img>
                                 </a>
                             </div>
                             <div className={cx('info-gt')}>
-                                <h4 className={cx('name-gt', 'text-uppercase')}>
-                                    <a href="#" className={cx('clblack')}>
+                                <h4 className={cx('name-gt')}>
+                                    <a href="#" className={cx('clblack', 'text-uppercase')}>
                                         THẨM ĐỊNH GIÁ DOANH NGHIỆP
                                     </a>
                                 </h4>
                             </div>
                         </div>
                     </div>
-                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')}>
-                        <div className={cx('list-gt', 'aos-init', 'aos-animate')} data-aos="flip-left">
+                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')} data-aos="flip-left">
+                        <div className={cx('list-gt', 'aos-init', 'aos-animate')}>
                             <div className={cx('eff-scale')}>
                                 <a href="#">
-                                    <img></img>
+                                    <img src="https://www.sbb.ch/content/dam/internet/sharedimages/personen/GA-Hund.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg"></img>
                                 </a>
                             </div>
                             <div className={cx('info-gt')}>
-                                <h4 className={cx('name-gt', 'text-uppercase')}>
-                                    <a href="#" className={cx('clblack')}>
+                                <h4 className={cx('name-gt')}>
+                                    <a href="#" className={cx('clblack', 'text-uppercase')}>
                                         THẨM ĐỊNH GIÁ TRỊ VÔ HÌNH
                                     </a>
                                 </h4>
                             </div>
                         </div>
                     </div>
-                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')}>
-                        <div className={cx('list-gt', 'aos-init', 'aos-animate')} data-aos="flip-left">
+                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')} data-aos="flip-left">
+                        <div className={cx('list-gt', 'aos-init', 'aos-animate')}>
                             <div className={cx('eff-scale')}>
                                 <a href="#">
-                                    <img></img>
+                                    <img src="https://www.sbb.ch/content/dam/internet/sharedimages/personen/GA-Hund.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg"></img>
                                 </a>
                             </div>
                             <div className={cx('info-gt')}>
-                                <h4 className={cx('name-gt', 'text-uppercase')}>
-                                    <a href="#" className={cx('clblack')}>
+                                <h4 className={cx('name-gt')}>
+                                    <a href="#" className={cx('clblack', 'text-uppercase')}>
                                         TƯ VẤN ĐẦU TƯ
                                     </a>
                                 </h4>
                             </div>
                         </div>
                     </div>
-                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')}>
-                        <div className={cx('list-gt', 'aos-init', 'aos-animate')} data-aos="flip-left">
+                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')} data-aos="flip-left">
+                        <div className={cx('list-gt', 'aos-init', 'aos-animate')}>
                             <div className={cx('eff-scale')}>
                                 <a href="#">
-                                    <img></img>
+                                    <img src="https://www.sbb.ch/content/dam/internet/sharedimages/personen/GA-Hund.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg"></img>
                                 </a>
                             </div>
                             <div className={cx('info-gt')}>
-                                <h4 className={cx('name-gt', 'text-uppercase')}>
-                                    <a href="#" className={cx('clblack')}>
+                                <h4 className={cx('name-gt')}>
+                                    <a href="#" className={cx('clblack', 'text-uppercase')}>
                                         TƯ VẤN BẤT ĐỘNG SẢN
                                     </a>
                                 </h4>
                             </div>
                         </div>
                     </div>
-                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')}>
-                        <div className={cx('list-gt', 'aos-init', 'aos-animate')} data-aos="flip-left">
+                    <div className={cx('col-md-4', 'col-sm-12', 'col-xs-12')} data-aos="flip-left">
+                        <div className={cx('list-gt', 'aos-init', 'aos-animate')}>
                             <div className={cx('eff-scale')}>
                                 <a href="#">
-                                    <img></img>
+                                    <img src="https://www.sbb.ch/content/dam/internet/sharedimages/personen/GA-Hund.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg"></img>
                                 </a>
                             </div>
                             <div className={cx('info-gt')}>
-                                <h4 className={cx('name-gt', 'text-uppercase')}>
-                                    <a href="#" className={cx('clblack')}>
+                                <h4 className={cx('name-gt')}>
+                                    <a href="#" className={cx('clblack', 'text-uppercase')}>
                                         CUNG CẤP THÔNG TIN KINH TẾ
                                     </a>
                                 </h4>
