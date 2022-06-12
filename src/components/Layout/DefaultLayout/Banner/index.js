e72aea6f59a5e879aa4e981c2dcc4d77b04dc265
Hoang Nghia Viet <viethoangnghia@gmail.com>
2022-06-12 20:51:40 +0700

    Update banner

--- src/components/Layout/DefaultLayout/Banner/index.js
+++ src/components/Layout/DefaultLayout/Banner/index.js
@@ -1,18 +1,20 @@
 import classNames from 'classnames/bind';
 import styles from './Banner.module.scss';
+import img from '../../../../assets/images/banner.jpg';
 
 const cx = classNames.bind(styles);
 
-function Banner({content}) {
+function Banner({ content }) {
     return (
         <div className={cx('bannerPage')}>
-            <img
-                src={
-                    'https://png.pngtree.com/thumb_back/fh260/back_our/20190623/ourmid/pngtree-summer-outdoor-scene-banner-image_249780.jpg'
-                }
-                className={cx('bannerImage')}
-            />
-            <h3 className={cx('titleLienHe')}>{content}</h3>
+            <img src={img} className={cx('bannerImage')} />
+            <h3 className={cx('titleLienHe')} data-aos="zoom-in">
+                {content}
+                <br></br>
+                <div className={cx('subtitle')}>
+                    <a href="/">TRANG CHỦ</a> / {content}
+                </div>
+            </h3>
         </div>
     );
 }
